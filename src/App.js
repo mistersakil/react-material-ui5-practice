10d1dce0cf08a0556f207870f2d0e1be6257f2f4
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-10-21 14:35:22 +0600

    first commit and started working on material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from "@mui/material";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>learning material ui</h1>
+      <Button>default</Button>
+      <Button color="info">info</Button>
+      <Button color="success" variant="contained">
+        info
+      </Button>
+      <Button color="error" variant="contained" disabled>
+        info
+      </Button>
+      <Button
+        color="error"
+        variant="contained"
+        href="https://google.com"
+        target="_blank"
+      >
+        info
+      </Button>
+    </>
   );
 }
 
